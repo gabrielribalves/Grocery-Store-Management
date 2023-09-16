89da1e65b230a4d1a09d34ae2d7d3602409a98d0
Gabriel Ribeiro <gabriel.ribalves@gmail.com>
2023-09-16 16:23:52 -0300

    Fist commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Grocery Store Management
+
+This application Uses Python and SQL
+It can manage the store and the staff
+
+"""
+__version__ = "0.0.1"
+__license__ = "Unlicense"
+
+
+
+def main(product):
+    print(f"{product.name}   {product.price}")
